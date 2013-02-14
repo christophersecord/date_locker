5d071d524c192725aa449aa0f085fecf698bff72
christophersecord <christopher.secord@gmail.com>
2013-02-14 15:06:28 -0600

    sp work

--- sql/sp/getAvailability.sql
+++ sql/sp/getAvailability.sql
@@ -5,40 +5,35 @@ DELIMITER //
 create procedure dl_getAvailability(in selectedDay date)
 begin
 
+  declare pStart datetime default selectedDay;
+  declare pEnd datetime;
+
   create temporary table availability (
     startTime datetime not null
   );
 
   set @i:= 0;
-  set @availabilityDate:= str_to_date('1900-01-01 00','%Y-%m-%d %H');
-  set @realDate:= str_to_date(concat(year(selectedDay),'-',month(selectedDay),'-',day(selectedDay),' 00'),'%Y-%m-%d %H');
 
   -- Really? This is how you do a loop in MySQL? Seriously?
   -- loop through all time blocks in the current day
   addRows: loop
 
-    if exists (
-      select * from dl_availability
-      where
-        startAvailability <= @availabilityDate
-        and endAvailability > @availabilityDate
-    ) and not exists (
-      select * from dl_appointment
-      where
-        @realDate between startTime and endTime
-    ) then
+    -- is the smallest possible appointment available at this time?
+    set pEnd = date_add(pStart, interval 30 minute);
+    if dl_isAvailable(pStart,pEnd)
+    then 
 
       insert availability
-      values (@realDate);
+      values (pStart);
  
     end if;
 
     set @i:= @i + 1;
 
     -- TODO: this loops in 30 minute steps. It should be configurable
-    set @realDate:= date_add(@realDate, interval 30 minute);
-    set @availabilityDate:= date_add(@availabilityDate, interval 30 minute);
+    set pStart = date_add(pStart, interval 30 minute);
 
+    -- this is how you end a loop in MySQL??
     if @i >= 48 then
       leave addRows;
     end if;
--- sql/sp/isAvailable.sql
+++ sql/sp/isAvailable.sql
@@ -6,15 +6,26 @@ create function dl_isAvailable(aStartTime datetime, aEndTime datetime)
 returns bit
 begin
 
-  set @aStartTime:= str_to_date(concat('1900-01-01 ',hour(aStartTime),':',minute(aStartTime)),'%Y-%m-%d %H:%i');
-  set @aEndTime:= str_to_date(concat('1900-01-01 ',hour(aEndTime),':',minute(aEndTime)),'%Y-%m-%d %H:%i');
+  declare pStart datetime
+  default str_to_date(concat('1900-01-01 ',hour(aStartTime),':',minute(aStartTime)),'%Y-%m-%d %H:%i');
+  
+  declare pEnd datetime 
+  default str_to_date(concat('1900-01-01 ',hour(aEndTime),':',minute(aEndTime)),'%Y-%m-%d %H:%i');
+
+  -- end time must be greater than start time
+  if aEndTime <= aStartTime then
+    return 0;
+  end if;
+
+  -- TODO: startTime must start on an hour or half-hour boundry
+  -- TODO: appointment length must be an approved time
 
   if exists (
     -- does the proposed appointment fit entirely within an 
     select * from dl_availability
     where
-      startAvailability <= @aStartTime
-      and endAvailability >= @aEndDate
+      startAvailability <= pStart
+      and endAvailability >= pEnd
       and dayOfWeek = dayOfWeek(aStartTime)
   ) and not exists (
     -- is there no other appointment that conflicts with it
@@ -29,5 +40,6 @@ begin
   end if;
 
   return 0;
+
 
 end //
