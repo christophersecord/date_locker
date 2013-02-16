10b74fe8ab3941142b2aa66746e6e78eee02bfa6
christophersecord <christopher.secord@gmail.com>
2013-02-15 19:24:03 -0600

    bug fixes in isAvailable. Added SP to clear timed-out appointment locks

--- sql/sp/isAvailable.sql
+++ sql/sp/isAvailable.sql
@@ -4,9 +4,10 @@ DELIMITER //
 
 /** isAvailable
  * @hint returns true if the proposed time block is available as an appointment and is a valid appointment
+ * for testing purposes, returns 0 when an appointment is available and a number otherwise
  */
 create function dl_isAvailable(aStartTime datetime, aEndTime datetime)
-returns bit
+returns int
 begin
 
   declare pStart datetime
@@ -14,7 +15,7 @@ begin
 
   -- note, the end time is set to one minute before the input end time so that midnight isn't seen as the next day
   declare pEnd datetime 
-  default str_to_date(concat('1900-01-01 ',hour(aEndTime),':',minute(aEndTime)-1),'%Y-%m-%d %H:%i');
+  default date_add(str_to_date(concat('1900-01-01 ',hour(aEndTime),':',minute(aEndTime)),'%Y-%m-%d %H:%i'),interval -1 minute);
 
   declare pStartTimeOffset int;
 
@@ -26,12 +27,12 @@ begin
   where varName='apt_min_advance';
 
   if hour(timeDiff(aStartTime,now())) < duration then
-    return 0;
+    return 1;
   end if;
 
   -- end time must be greater than start time and be on the same day
   if (aEndTime <= aStartTime) || (day(aStartTime) != day(aEndTime)) then
-    return 0;
+    return 2;
   end if;
 
   -- startTime must start on an hour or half-hour boundry
@@ -40,19 +41,20 @@ begin
   from dl_config where varName='apt_start_times';
 
   if pStartTimeOffset != 0 then
-    return 0;
+    return 3;
   end if;
 
   -- appointment length must be an approved duration
   if not exists (
     select *
     from dl_appointmentDuration
-    where minutes = minute(timeDiff(aStartTime,aEndTime))
+    where minutes = minute(timeDiff(aStartTime,aEndTime)) + hour(timeDiff(aStartTime,aEndTime))*60
   ) then
-    return 0;
+    return 4;
   end if;
 
   -- appointment must be on an allowed time block
+/*
   if exists (
     -- does the proposed appointment fit entirely within a designated availability block
     select * from dl_availability
@@ -64,20 +66,52 @@ begin
     -- is there no other appointment that conflicts with it
     select * from dl_appointment
     where
-      aStartTime between startTime and endTime
-      or aEndTime between startTime and endTime
+      (startTime < aStartTime and aStartTime < endTime) -- can't use "between" operator here because it matches <=
+      or (startTime < aEndTime and aEndTime < endTime)
 
   ) and not exists (
     -- this timeblock is not locked by another user
     select * from dl_appointmentLock
     where
-      aStartTime between startTime and endTime
-      or aEndTime between startTime and endTime
+      (startTime < aStartTime and aStartTime < endTime) -- can't use "between" operator here either
+      or (startTime < aEndTime and aEndTime < endTime)
   ) then
 
     return 1;
 
   end if;
+*/ 
+-- remove before flight
+  if not exists (
+    -- does the proposed appointment fit entirely within a designated availability block
+    select * from dl_availability
+    where
+      startAvailability <= pStart
+      and endAvailability >= pEnd
+      and dayOfWeek = dayOfWeek(aStartTime)
+  ) then
+
+    return 5;
+  end if;
+if exists (
+    -- is there no other appointment that conflicts with it
+  select * from dl_appointment
+    where
+      aStartTime between startTime and endTime
+      or aEndTime between startTime and endTime
+) then
+  return 6;
+end if;
+if exists (
+    -- this timeblock is not locked by another user
+    select * from dl_appointmentLock
+    where
+      (startTime < aStartTime and aStartTime < endTime) -- can't use "between" operator here either
+      or (startTime < aEndTime and aEndTime < endTime)
+) then
+  return 7;
+end if;
+
 
   return 0;
 
---
+++ sql/sp/lockCleanup.sql
@@ -0,0 +1,49 @@
+drop procedure if exists dl_lockCleanup;
+
+DELIMITER //
+
+/** lockCleanup
+ * @hint deletes timed-out locks. Truncates the table (for performance reasons) if no valid locks remain
+ */
+create procedure dl_lockCleanup()
+begin
+
+  -- this temp table to hold lockIDs is necessary to avoid MySQL error 1093
+  create temporary table lockIDs (
+    lockID int not null primary key
+  );
+
+  insert lockIDs (lockID)
+  select lockID
+  from dl_appointmentLock, dl_config
+  where
+    varName = 'apt_lock_time'
+    and (
+      -- delete locks older than 'apt_lock_time' minutes
+      hour(timeDiff(now(),bookedOn))*60 + minute(timeDiff(now(),bookedOn)) > intVal
+      -- delete locks older than a day
+      or dateDiff(now(),bookedOn) > 1
+    );
+
+  -- I'm not sure why the delete statement below triggers the safe update error, since it uses a key column
+  SET SQL_SAFE_UPDATES=0;
+
+  delete
+  from dl_appointmentLock
+  where lockID in (
+    select lockID
+    from lockIDs
+  );
+
+  -- truncate table if it's empty
+/*
+  if not exists (
+    select * from dl_appointmentLock
+  ) then
+  
+    truncate dl_appointmentLock;
+
+  end if;
+*/
+  drop table lockIDs;
+end //
