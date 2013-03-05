ab5558a24718c706bcdf1b30bf05257f0d699b83
christophersecord <christopher.secord@gmail.com>
2013-03-04 22:52:50 -0600

    added appointmentGet

---
+++ sql/sp/appointmentGet.sql
@@ -0,0 +1,24 @@
+/** stored procedure for dana_lupo project
+ * @author christophersecord
+ * @date 20130304
+ * @language SQL
+ * @platform mySQL
+ */
+drop procedure if exists dl_appointmentGet;
+
+DELIMITER //
+
+/** appointmentGet
+ * returns a query containing all information about an appointment
+ */
+create procedure dl_appointmentGet (
+  in pAppointmentID int
+)
+begin
+
+  select *
+  from dl_appointment
+  where appointmentID = pAppointmentID;
+ 
+end //
+
