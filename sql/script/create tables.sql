2e6fdc0afd02a9c2aab4d439e96d427d9c657c5c
csecord <csecord@uwf.edu>
2023-09-10 15:21:19 -0500

    allow null passwords to indicate guest accounts

--- sql/script/create tables.sql
+++ sql/script/create tables.sql
@@ -17,10 +17,10 @@ drop table if exists dl_config;
  * @hint application config values
  */
 create table dl_config (
-  varName char(15) not null primary key,
-  intVal int null,
-  charVal varchar(50) null,
-  dateVal datetime null
+	varName char(15) not null primary key,
+	intVal int null,
+	charVal varchar(50) null,
+	dateVal datetime null
 );
 /** add default config values
  */
@@ -49,22 +49,24 @@ insert dl_config (varName,intVal) values ('apt_lock_time',720);
  * @hint one row per client that has scheduled an appointment.
  */
 create table dl_client (
-  clientID int not null auto_increment primary key,
-  emailAddress varchar(250) not null,
-  passwd char(56) not null, -- stores SHA-2 hashes at 224 bits
+	clientID int not null auto_increment primary key,
+	emailAddress varchar(250) not null,
 
-  phoneNumber char(10) null,
+	-- passwd is a SHA-2 hash. null indicates a guest account that cannot log in
+	passwd char(56) null,
 
-  dateCreated timestamp not null default now(),
+	phoneNumber char(10) null,
 
-  unique(emailAddress)
+	dateCreated timestamp not null default now(),
+
+	unique(emailAddress)
 );
 
 /** appointmentDuration
  * @hint allowable durations, in minutes, of an appointment
  */
 create table dl_appointmentDuration (
-  minutes tinyint not null primary key
+	minutes tinyint not null primary key
 );
 insert dl_appointmentDuration values (30);
 insert dl_appointmentDuration values (60);
@@ -74,67 +76,67 @@ insert dl_appointmentDuration values (90);
  * @hint the hours on each day of the week when appointments can be made
  */
 create table dl_businessHours (
-  dayOfWeek int not null,
-  startAvailability datetime not null,
-  endAvailability datetime not null
+	dayOfWeek int not null,
+	startAvailability datetime not null,
+	endAvailability datetime not null
 );
 
 /** appointment
  * @hint one row per scheduled appointment or blocked time
  */
 create table dl_appointment (
-  appointmentID int not null auto_increment primary key,
-  clientID int not null,
+	appointmentID int not null auto_increment primary key,
+	clientID int not null,
 
-  startTime datetime not null,
-  endTime datetime not null,
+	startTime datetime not null,
+	endTime datetime not null,
 
-  bookedOn timestamp not null default now(),
+	bookedOn timestamp not null default now(),
 
-  googleCalendarGUID varchar(250) null,
-  memo varchar(250) null,
+	googleCalendarGUID varchar(250) null,
+	memo varchar(250) null,
 
-  foreign key(clientID) references dl_client(clientID)
+	foreign key(clientID) references dl_client(clientID)
 );
 
 /** appointmentLock
  * @hint temporarily locks a block of time so that a user can log in or pay or whatever
  */
 create table dl_appointmentLock (
-  lockID int not null auto_increment primary key,
-  lockToken char(36) not null,
+	lockID int not null auto_increment primary key,
+	lockToken char(36) not null,
 
-  startTime datetime not null,
-  endTime datetime not null,
+	startTime datetime not null,
+	endTime datetime not null,
 
-  bookedOn timestamp not null default now()
+	bookedOn timestamp not null default now()
 );
 
 /** appointmentAvailabilityBlock
  * @hint blocks off time when no appointments should be allowed, or when appointments are allowed outside business hours 
  */
 create table dl_appointmentAvailabilityBlock (
-  blockID int not null auto_increment primary key,
-  appointmentsAllowed bit not null default 0,
+	blockID int not null auto_increment primary key,
+	appointmentsAllowed bit not null default 0,
 
-  startTime datetime not null,
-  endTime datetime not null,
+	startTime datetime not null,
+	endTime datetime not null,
 
-  googleCalendarGUID varchar(250) null,
-  memo varchar(250) null
+	googleCalendarGUID varchar(250) null,
+	memo varchar(250) null
 );
 
 /** login
  * @hint stores login information
  */
 create table dl_login (
-  loginID int not null auto_increment primary key,
-  loginToken char(36) not null,
+	loginID int not null auto_increment primary key,
+	loginToken char(36) not null,
 
-  clientID int not null,
+	clientID int not null,
 
-  loginTime datetime not null,
-  logoutTime datetime not null,
+	loginTime datetime not null,
+	logoutTime datetime not null,
 
-  foreign key(clientID) references dl_client(clientID)
+	foreign key(clientID) references dl_client(clientID)
 );
