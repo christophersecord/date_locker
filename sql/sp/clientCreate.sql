c0e408ef4a910e8e5b639f792d67c0178a552c9a
csecord <csecord@uwf.edu>
2023-09-11 22:45:49 -0500

    allow guest accounts

--- sql/sp/clientCreate.sql
+++ sql/sp/clientCreate.sql
@@ -12,21 +12,29 @@ DELIMITER //
  * creates a client
  */
 create procedure dl_clientCreate (
-  in pEmailAddress varchar(250),
-  in plainTextPasswd varchar(250),
+	in pEmailAddress varchar(250),
+	in plainTextPasswd varchar(250),
 
-  out clientID int
+	out clientID int
 )
 begin
 
-  if not exists (
-    select * from dl_client where emailAddress = pEmailAddress
-  ) then
+	if not exists (
+		select * from dl_client where emailAddress = pEmailAddress
+	) then
 
-    insert dl_client (emailAddress, passwd)
-    values (pEmailAddress,sha2(plainTextPasswd,224));
+		-- create the client as a real client (with a password)
+		-- or a guest account (with a null password)
+		if (plainTextPasswd == "") then
 
-    set clientID = last_insert_id();
-  end if;
+			insert dl_client (emailAddress)
+			values (pEmailAddress);
+		else
+			insert dl_client (emailAddress, passwd)
+			values (pEmailAddress,sha2(plainTextPasswd,224));
+		end if;
+
+		set clientID = last_insert_id();
+	end if;
 end //
 
